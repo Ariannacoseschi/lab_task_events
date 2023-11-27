31dcbd0d4572a8caa0ac7ffa81daf360a8949825
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-27 15:27:17 +0000

    added delete button to remove items added to list

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,40 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("#todo-form");
-    const newToDo = document.querySelector("#new-todo");
-    const list = document.querySelector("#list");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        if (newToDo.value.trim() !== "") {
-            // Create a new list item
-            const newListItem = document.createElement("li");
-
-            // Set its text content to the input value
-            newListItem.innerText = newToDo.value;
-
-            // Create a delete button for the list item
-            const deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
-            deleteButton.addEventListener("click", function () {
-                // Remove the corresponding list item when the delete button is clicked
-                newListItem.remove();
-            });
-
-            // Append the delete button to the list item
-            newListItem.appendChild(deleteButton);
-
-            // Append the new list item to the existing list
-            list.appendChild(newListItem);
-
-            // Clear the input field after adding the item
-            newToDo.value = "";
-        }
+const button = document.querySelector("#enter");
+const list = document.querySelector("#list");
+const newToDo = document.querySelector("#new-todo");
+document.querySelector("#enter").addEventListener("click", function (event) {
+    event.preventDefault();
+    const newListItem = document.createElement("li");
+    newListItem.innerText = newToDo.value;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", function(){
+        newListItem.remove();
     });
+    newListItem.appendChild(deleteButton);
+    list.appendChild(newListItem);
+    newToDo.value = "";
 });
-
-
-
-
-
-
+// document.addEventListener("DOMContentLoaded", function () {
+//     const button = document.querySelector("#enter");
+//     const list = document.querySelector("#list");
+//     const newToDo = document.querySelector("#new-todo");
+//     document.querySelector("#todo-form").addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         const newListItem = document.createElement("li");
+//         newListItem.innerText = newToDo.value;
+//         list.appendChild(newListItem);
+//         newToDo.value = "";
+//     });
+// });
