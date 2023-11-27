35133a71833a1ac74462debb44f5e9351cef9704
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-27 14:38:35 +0000

    can grab user input

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,7 +1,10 @@
 const button = document.querySelector("#enter");
-
+const toDoList = document.querySelector("#list");
+const newToDo = document.querySelector("#new-todo");
 
 button.addEventListener("click", () =>{
-    console.log("button clicked");
+    //console.log("button clicked");
+    const newListItem = document.createElement("li");
+    newListItem.innerText = newToDo;
 });
 
